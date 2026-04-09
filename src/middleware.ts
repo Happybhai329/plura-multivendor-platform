bec305b43f125bebb9693166be58854815afd7fa
Happybhai329 <bhasinhappy0506@gmail.com>
2026-04-09 15:59:44 +0530

    fix clerk middleware v4

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/", "/site", "/api/uploadthing"],
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
