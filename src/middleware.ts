97fd871ccfa842bf7577ace36702bbfa7edd0b90
Happybhai329 <bhasinhappy0506@gmail.com>
2026-04-09 16:14:07 +0530

    fix routing + auth flow

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/site", "/api/uploadthing"],
+  publicRoutes: ["/", "/site", "/api/uploadthing", "/agency/sign-in", "/agency/sign-up"],
 });
 
 export const config = {
